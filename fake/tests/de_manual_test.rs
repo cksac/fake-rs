177373e06d2f3c6e05a9796442d17959f66c189b
Till von Ahnen <xoryouyou@googlemail.com>
2021-12-18 16:47:28 +0100

    Changed address format and println output

--- fake/tests/de_manual_test.rs
+++ fake/tests/de_manual_test.rs
@@ -1,12 +1,15 @@
+use fake::faker::address::de_de::SecondaryAddress;
 use fake::faker::address::raw::{CityName, StateName, StreetName, ZipCode};
 use fake::faker::company::raw::CompanyName;
 use fake::locales::DE_DE;
 use fake::Fake;
+
 #[allow(unused)]
 #[derive(Debug)]
 pub struct Company {
     name: String,
-    street: String,
+    address_line_1: String,
+    address_line_2: String,
     zipcode: String,
     city: String,
     state: String,
@@ -14,12 +17,19 @@ pub struct Company {
 
 #[test]
 fn de_manual_test() {
-    let c = Company {
-        name: CompanyName(DE_DE).fake(),
-        street: StreetName(DE_DE).fake(),
-        zipcode: ZipCode(DE_DE).fake(),
-        city: CityName(DE_DE).fake(),
-        state: StateName(DE_DE).fake(),
-    };
-    println!("{:?}", c);
+    for _ in 1..10 {
+        let c = Company {
+            name: CompanyName(DE_DE).fake(),
+            address_line_1: format!(
+                "{} {}",
+                StreetName(DE_DE).fake::<String>(),
+                (1..100).fake::<u16>()
+            ),
+            address_line_2: SecondaryAddress().fake(),
+            zipcode: ZipCode(DE_DE).fake(),
+            city: CityName(DE_DE).fake(),
+            state: StateName(DE_DE).fake(),
+        };
+        println!("{:#?}", c);
+    }
 }
