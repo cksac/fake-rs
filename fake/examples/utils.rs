19241d4e404521c37a6251c3a966045d17603d68
Thomas Duindam <thomas.duindam@tribusmed.com>
2024-03-18 09:23:47 +0100

    Update glam to 0.26

--- fake/examples/utils.rs
+++ fake/examples/utils.rs
@@ -6,11 +6,13 @@ use fake::{
 
 #[derive(Debug, Dummy)]
 struct Foo {
+    #[allow(unused)]
     #[dummy(faker = "either(PhoneNumber(), CellNumber())", wrapper = "WrappedVal")]
     phone_number: String,
 }
 
 fn main() {
+    #[allow(clippy::disallowed_names)]
     for _ in 0..10 {
         let foo: Foo = Faker.fake();
         println!("{:?}", foo);
