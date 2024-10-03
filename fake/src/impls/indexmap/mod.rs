627bc78e46a85a48ba096ae8a2e3fce500b6efe5
Haf <haf@protonmail.ch>
2024-10-03 12:09:35 +0200

    feat: support for IndexSet

--- fake/src/impls/indexmap/mod.rs
+++ fake/src/impls/indexmap/mod.rs
@@ -1,5 +1,5 @@
 use crate::{Dummy, Fake, Faker};
-use indexmap::IndexMap;
+use indexmap::{IndexMap, IndexSet};
 use rand::Rng;
 use std::hash::{BuildHasher, Hash};
 
@@ -16,6 +16,21 @@ where
         let mut m = IndexMap::with_capacity_and_hasher(len, S::default());
         for _ in 0..len {
             m.insert(config.fake_with_rng(rng), config.fake_with_rng(rng));
+        }
+        m
+    }
+}
+
+impl<T, S> Dummy<Faker> for IndexSet<T, S>
+where
+    T: Dummy<Faker> + Hash + Eq,
+    S: BuildHasher + Default,
+{
+    fn dummy_with_rng<R: Rng + ?Sized>(config: &Faker, rng: &mut R) -> Self {
+        let len = get_len(config, rng);
+        let mut m = IndexSet::with_capacity_and_hasher(len, S::default());
+        for _ in 0..len {
+            m.insert(config.fake_with_rng(rng));
         }
         m
     }
